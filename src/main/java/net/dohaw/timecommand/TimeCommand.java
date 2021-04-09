5883031002dc1efd9a302922656f19445483c03c
c10coding <caleb.ja.owens@gmail.com>
2021-04-09 12:44:13 -0400

    Fixed the error that occurred when typing /tc - v1.0.1

--- src/main/java/net/dohaw/timecommand/TimeCommand.java
+++ src/main/java/net/dohaw/timecommand/TimeCommand.java
@@ -8,13 +8,7 @@ import org.bukkit.conversations.Conversation;
 import org.bukkit.conversations.ConversationFactory;
 import org.bukkit.entity.Player;
 
-import java.text.ParseException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 public class TimeCommand implements CommandExecutor {
@@ -29,54 +23,53 @@ public class TimeCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender.hasPermission("timecommand.use")){
             /* /timecommand create <time in military> .... */
-            if(args[0].equalsIgnoreCase("create") && args.length > 2){
-                String timeStr = args[1];
-                if(timeStr.contains(":")){
+            if(args.length > 0){
+                if(args[0].equalsIgnoreCase("create") && args.length > 2){
+                    String timeStr = args[1];
+                    if(timeStr.contains(":")){
 
-                    String[] timeSplit = timeStr.split(":");
-                    int hour = Integer.parseInt(timeSplit[0]);
-                    int minute = Integer.parseInt(timeSplit[1]);
-                    String[] commandArgs = Arrays.copyOfRange(args, 2, args.length);
-                    String desiredCommand = String.join(" ", commandArgs);
+                        String[] timeSplit = timeStr.split(":");
+                        int hour = Integer.parseInt(timeSplit[0]);
+                        int minute = Integer.parseInt(timeSplit[1]);
+                        String[] commandArgs = Arrays.copyOfRange(args, 2, args.length);
+                        String desiredCommand = String.join(" ", commandArgs);
 
-                    plugin.addTimeCommand(new TimeCommandInfo(desiredCommand, hour, minute));
-                    ResponderFactory rf = new ResponderFactory(sender);
-                    rf.sendMessage("A new time command has been made!");
-                    rf.sendMessage("This command will run at &6" + TimeUtil.format(hour, minute) + "&f: " + "/&b" + desiredCommand);
+                        plugin.addTimeCommand(new TimeCommandInfo(desiredCommand, hour, minute));
+                        ResponderFactory rf = new ResponderFactory(sender);
+                        rf.sendMessage("A new time command has been made!");
+                        rf.sendMessage("This command will run at &6" + TimeUtil.format(hour, minute) + "&f: " + "/&b" + desiredCommand);
 
-                }else{
-                    sender.sendMessage("This is not a valid time!");
-                }
-            /* /timecommand list - Lists the time commands */
-            }else if(args[0].equalsIgnoreCase("list")){
-                if(hasTimeCommands()){
-                    listTimeCommands(sender, false);
-                }else{
-                    sender.sendMessage("There are no time commands!");
-                }
-            }else if(args[0].equalsIgnoreCase("delete")){
-
-                if(hasTimeCommands()){
-                    if(sender instanceof Player){
-                        listTimeCommands(sender, true);
-                        ConversationFactory cf = new ConversationFactory(plugin);
-                        Conversation conv = cf.withFirstPrompt(new TimeCommandDeletePrompt(plugin)).withLocalEcho(false).buildConversation((Player)sender);
-                        conv.begin();
                     }else{
-                        sender.sendMessage("Only players can use this command! This is because it prompts you in game upon which commands you want to delete via their numbers");
-                        sender.sendMessage("If you wish to delete time commands without going in game, you can stop the server, and delete the time commands through the config");
+                        sender.sendMessage("This is not a valid time!");
                     }
-                }else{
-                    sender.sendMessage("There are no time commands!");
-                }
+                    /* /timecommand list - Lists the time commands */
+                }else if(args[0].equalsIgnoreCase("list")){
+                    if(hasTimeCommands()){
+                        listTimeCommands(sender, false);
+                    }else{
+                        sender.sendMessage("There are no time commands!");
+                    }
+                }else if(args[0].equalsIgnoreCase("delete")){
 
-            }else if(args[0].equalsIgnoreCase("help")){
-                ResponderFactory rf = new ResponderFactory(sender);
-                rf.sendMessage("&2This plugin allows you to have specific commands ran at specific times in the day");
-                rf.sendMessage("\n&6TimeCommand commands: ");
-                rf.sendMessage("&2- /tco list &f - Lists all the time commands");
-                rf.sendMessage("&2- /tco create <hour:minute> <command> &f - Allows you to create a time command");
-                rf.sendMessage("&2- /tco delete &f - Allows you to delete a time command");
+                    if(hasTimeCommands()){
+                        if(sender instanceof Player){
+                            listTimeCommands(sender, true);
+                            ConversationFactory cf = new ConversationFactory(plugin);
+                            Conversation conv = cf.withFirstPrompt(new TimeCommandDeletePrompt(plugin)).withLocalEcho(false).buildConversation((Player)sender);
+                            conv.begin();
+                        }else{
+                            sender.sendMessage("Only players can use this command! This is because it prompts you in game upon which commands you want to delete via their numbers");
+                            sender.sendMessage("If you wish to delete time commands without going in game, you can stop the server, and delete the time commands through the config");
+                        }
+                    }else{
+                        sender.sendMessage("There are no time commands!");
+                    }
+
+                }else if(args[0].equalsIgnoreCase("help")){
+                    sendHelp(sender);
+                }
+            }else{
+                sendHelp(sender);
             }
         }
         return false;
@@ -100,6 +93,15 @@ public class TimeCommand implements CommandExecutor {
 
             rf.sendMessage(line);
         }
+    }
+
+    private void sendHelp(CommandSender sender){
+        ResponderFactory rf = new ResponderFactory(sender);
+        rf.sendMessage("&2This plugin allows you to have specific commands ran at specific times in the day");
+        rf.sendMessage("\n&6TimeCommand commands: ");
+        rf.sendMessage("&2- /tco list &f - Lists all the time commands");
+        rf.sendMessage("&2- /tco create <hour:minute> <command> &f - Allows you to create a time command");
+        rf.sendMessage("&2- /tco delete &f - Allows you to delete time commands");
     }
 
     private boolean hasTimeCommands(){
